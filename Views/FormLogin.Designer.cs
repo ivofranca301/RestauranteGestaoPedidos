1e56b5d3661a939a3bcf2fc92fb1d51e6efd4664
msssantos <martaa.santoos@gmail.com>
2025-04-16 23:11:43 +0100

    Fase 1: Marta - Implementação das interfaces de login e nova conta (FormLogin, FormNovaConta)

---
+++ Views/FormLogin.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RestauranteGestaoPedidos.Views
+{
+    partial class FormLogin
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnNovaConta = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtEmail.Location = new System.Drawing.Point(58, 3);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(300, 27);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha:";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtSenha.Location = new System.Drawing.Point(58, 36);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(300, 27);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnLogin.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(58, 69);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 30);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnNovaConta
+            // 
+            this.btnNovaConta.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnNovaConta.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNovaConta.ForeColor = System.Drawing.Color.White;
+            this.btnNovaConta.Location = new System.Drawing.Point(164, 69);
+            this.btnNovaConta.Name = "btnNovaConta";
+            this.btnNovaConta.Size = new System.Drawing.Size(100, 30);
+            this.btnNovaConta.TabIndex = 5;
+            this.btnNovaConta.Text = "Nova Conta";
+            this.btnNovaConta.UseVisualStyleBackColor = false;
+            this.btnNovaConta.Click += new System.EventHandler(this.btnNovaConta_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtEmail, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtSenha, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnLogin, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnNovaConta, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(10);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 200);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormLogin";
+            this.Text = "Login - Restaurante";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnNovaConta;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
---
+++ Views/FormLogin.cs
@@ -0,0 +1,61 @@
+﻿using RestauranteGestaoPedidos.Controllers;
+using System;
+using System.Windows.Forms;
+
+namespace RestauranteGestaoPedidos.Views
+{
+    public partial class FormLogin : Form
+    {
+        private readonly ClienteController _controller;
+
+        public string EmailLogado { get; private set; }
+        public string PapelLogado { get; private set; }
+
+        public FormLogin(ClienteController controller)
+        {
+            InitializeComponent();
+            _controller = controller;
+            _controller.ResponderLogin += Controller_ResponderLogin;
+        }
+
+        private void Controller_ResponderLogin(object sender, (bool Sucesso, string Mensagem, string Papel) e)
+        {
+            if (e.Sucesso)
+            {
+                EmailLogado = txtEmail.Text.Trim().ToLower();
+                PapelLogado = e.Papel;
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show(e.Mensagem, "Erro");
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var email = txtEmail.Text.Trim().ToLower();
+            var senha = txtSenha.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(senha))
+            {
+                MessageBox.Show("Preencha todos os campos.", "Erro");
+                return;
+            }
+
+            _controller.Login(email, senha);
+        }
+
+        private void btnNovaConta_Click(object sender, EventArgs e)
+        {
+            using (var formNovaConta = new FormNovaConta(_controller))
+            {
+                if (formNovaConta.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show("Por favor, faça login com sua nova conta.", "Sucesso");
+                }
+            }
+        }
+    }
+}
